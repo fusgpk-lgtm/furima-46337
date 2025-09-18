7c0a280eef6c85532486556350c9c3ad0f8130fa
fusgpk-lgtm <fumika.sgpk@gmail.com>
2025-09-18 15:39:59 +0900

    出品ページへの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to '/'
+  end
+
+  private
+
+  def item_params
+    prams.require(:item).permit(:item_name, :image, :explanation, :category_id, :state_id, :delivery_charge_id, :ship_from_id,
+                                :ship_day_id, :price)
+  end
 end
