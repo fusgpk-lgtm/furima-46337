f012d4580238aaaa26738dc72a091849e0821591
fusgpk-lgtm <fumika.sgpk@gmail.com>
2025-09-18 14:25:31 +0900

    itemテーブルにバリデーションを追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
+  validates :item_name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :state_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :ship_from_id, presence: true
+  validates :ship_day_id, presence: true
+  validates :price, presence: true
+
   belongs_to :user
 end
