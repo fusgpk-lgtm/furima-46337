7812d11a8c2a26290c2afad5f025920986fb5c76
fusgpk-lgtm <fumika.sgpk@gmail.com>
2025-09-18 17:19:33 +0900

    categoryにおけるActiveHashの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   validates :item_name, presence: true
   validates :explanation, presence: true
-  validates :category_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :state_id, presence: true
   validates :delivery_charge_id, presence: true
   validates :ship_from_id, presence: true
@@ -9,4 +9,5 @@ class Item < ApplicationRecord
   validates :price, presence: true
 
   belongs_to :user
+  belongs_to :category
 end
