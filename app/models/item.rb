3b8d09683d72da45b7bfb7f2d68d25affaac266b
fusgpk-lgtm <fumika.sgpk@gmail.com>
2025-09-18 17:57:41 +0900

    delivery_chargeにおけるActiveHashの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   validates :explanation, presence: true
   validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :state_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :delivery_charge_id, presence: true
+  validates :delivery_charge_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :ship_from_id, presence: true
   validates :ship_day_id, presence: true
   validates :price, presence: true
@@ -11,4 +11,5 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
   belongs_to :state
+  belongs_to :delivery_charge
 end
