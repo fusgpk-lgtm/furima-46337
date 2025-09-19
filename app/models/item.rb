855ce2e408fbe0251dbcf57d6edaa0c957454c42
fusgpk-lgtm <fumika.sgpk@gmail.com>
2025-09-19 14:18:39 +0900

    itemモデルの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   validates :item_name, presence: true
   validates :explanation, presence: true
   validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :state_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_charge_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :ship_from_id, presence: true
-  validates :ship_day_id, presence: true
+  validates :ship_from_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :ship_day_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: true
 
   belongs_to :user
