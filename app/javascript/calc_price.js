535a29e13092f4e95367e0c8c68dbc7c09bf001f
fusgpk-lgtm <fumika.sgpk@gmail.com>
2025-09-20 19:35:08 +0900

    エラーハンドリング後のJavaScriptの機能の修正

--- app/javascript/calc_price.js
+++ app/javascript/calc_price.js
@@ -1,17 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
+const calculatePrice = () => {
   const priceInput = document.getElementById("item-price");
-  if (!priceInput) return; // ページによっては要素が無いのでガード
+  if (!priceInput) {
+    return; // ページによっては要素が無いのでガード
+  }
 
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
 
     // 手数料計算 (10%)
-    const tax = Math.floor(inputValue * 0.1);
+    const addTaxPrice = Math.floor(inputValue * 0.1);
     // 利益計算
-    const profit = Math.floor(inputValue - tax);
+    const profit = Math.floor(inputValue - addTaxPrice);
 
     // 出力
-    document.getElementById("add-tax-price").innerHTML = isNaN(tax) ? "" : tax;
+    const taxElement = document.getElementById("add-tax-price");
+    const profitElement = document.getElementById("profit");
+    taxElement.innerHTML = isNaN(addTaxPrice) ? "" : addTaxPrice;
     document.getElementById("profit").innerHTML = isNaN(profit) ? "" : profit;
   });
-});
+};
+
+window.addEventListener('turbo:load', calculatePrice);
+window.addEventListener('turbo:render', calculatePrice);
