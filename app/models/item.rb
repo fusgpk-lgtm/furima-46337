41b30a0e67ef81c8b94518efbb5a916ed11ff6d3
fusgpk-lgtm <fumika.sgpk@gmail.com>
2025-09-20 19:26:28 +0900

    テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
+  validates :image, presence: true
   validates :item_name, presence: true
   validates :explanation, presence: true
   validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
@@ -9,6 +10,8 @@ class Item < ApplicationRecord
   validates :ship_from_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :ship_day_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: true
+  # 価格のバリデーションを数値関連に特化
+  validates_numericality_of :price, only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999
 
   belongs_to :user
   belongs_to :category
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name { Faker::Lorem.sentence }
+    explanation { Faker::Lorem.sentence }
+    category_id { Faker::Number.between(from: 2, to: 11) }
+    state_id { Faker::Number.between(from: 2, to: 7) }
+    delivery_charge_id { Faker::Number.between(from: 2, to: 3) }
+    ship_from_id { Faker::Number.between(from: 2, to: 48) }
+    ship_day_id { Faker::Number.between(from: 2, to: 4) }
+    price { Faker::Number.between(from: 300, to: 9_999_999) }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('spec/fixtures/files/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
