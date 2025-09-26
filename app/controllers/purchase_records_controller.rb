0e32d659ebe32a297bc8ad5f7cb94df6b5328afe
fusgpk-lgtm <fumika.sgpk@gmail.com>
2025-09-26 12:02:32 +0900

    フォームの情報を保存

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,6 +1,26 @@
 class PurchaseRecordsController < ApplicationController
+  before_action :authenticate_user!, except: :index
   def index
     @item = Item.find(params[:item_id])
     @purchase_form = PurchaseForm.new
+  end
+
+  def create
+    @item = Item.find_by(id: params[:item_id])
+    @purchase_form = PurchaseForm.new(purchase_form_params)
+    if @purchase_form.valid?
+      @purchase_form.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def purchase_form_params
+    params.require(:purchase_form).permit(:post_number, :prefecture_id, :city, :house_number, :house_name, :phone_number).merge(
+      user_id: current_user.id, item_id: @item.id
+    )
   end
 end
--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -8,15 +8,23 @@ class PurchaseForm
     validates :post_number, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :city
     validates :house_number
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input only numbers' }
-    validates :purchase_record
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input only numbers' },
+                             length: { in: 10..11, message: 'is too short' }
   end
 
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
-    purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
-    ShoppingInformation.create(post_number: post_number, prefecture_id: prefecture_id, city: city, house_number: house_number,
-                               house_name: house_name, phone_number: phone_number, purchase_record_id: purchase_record.id)
+    return false unless valid?
+
+    ActiveRecord::Base.transaction do
+      purchase_record = PurchaseRecord.create!(user_id: user_id, item_id: item_id)
+      ShoppingInformation.create!(post_number: post_number, prefecture_id: prefecture_id, city: city, house_number: house_number,
+                                  house_name: house_name, phone_number: phone_number, purchase_record_id: purchase_record.id)
+    end
+    true
+  rescue ActiveRecord::RecordInvalid => e
+    Rails.logger.error("PurchaseForm save failed: #{e.message}")
+    false
   end
 end
