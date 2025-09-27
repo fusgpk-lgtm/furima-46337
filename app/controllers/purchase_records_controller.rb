6ff8fe5ef148f9446f2ce17211422e6aaed2dc7a
fusgpk-lgtm <fumika.sgpk@gmail.com>
2025-09-27 22:34:54 +0900

    PAY.JPを利用したクレジットカード決済

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,17 +1,20 @@
 class PurchaseRecordsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
-  before_action :redirect_if_invalid_purchase, only: [:index, :create]
 
   def index
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @purchase_form = PurchaseForm.new
   end
 
   def create
     @purchase_form = PurchaseForm.new(purchase_form_params)
-    if @purchase_form.save
+    if @purchase_form.valid?
+      pay_item
+      @purchase_form.save
       redirect_to root_path
     else
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
       render :index, status: :unprocessable_entity
     end
   end
@@ -20,7 +23,7 @@ class PurchaseRecordsController < ApplicationController
 
   def purchase_form_params
     params.require(:purchase_form).permit(:post_number, :prefecture_id, :city, :house_number, :house_name, :phone_number, :token).merge(
-      user_id: current_user.id, item_id: @item.id
+      user_id: current_user.id, item_id: @item.id, token: params[:token]
     )
   end
 
@@ -28,10 +31,12 @@ class PurchaseRecordsController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
-  def redirect_if_invalid_purchase
-    # 出品者自身が購入しようとした場合、または商品がすでに売却済みの場合はトップページにリダイレクト
-    return unless current_user.id == @item.user_id || @item.purchase_record.present?
-
-    redirect_to root_path
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY'] # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price, # 商品の値段
+      card: purchase_form_params[:token], # カードトークン
+      currency: 'jpy' # 通貨の種類（日本円）
+    )
   end
 end
